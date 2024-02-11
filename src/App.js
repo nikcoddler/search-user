b60349ae7b1438dd3f85b60b654e5facae13ed82
Nikki Solanki <nikkisolanki@Nikkis-MacBook-Air.local>
2024-02-11 22:19:52 +0530

    created search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import SearchComponent from './components/serach-input';
+import Userlist from './components/user-list';
 
 function App() {
+
+  const [users, setUsers]  = useState([]);
+  const [filteredUsers, setFilteredUsers]  = useState([]);
+  const [inputValue, setInputValue] = useState();
+
+  const allUsers = useRef(null);
+
+  const fetchUsers = async (username) => {
+      try {
+        const res =  await fetch(`https://api.github.com/users`);
+      const data = await res.json();
+      const usersUrls = data.map((user) => user.url) 
+      getUserDetails(usersUrls);
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
+  const getUserDetails = async (usersUrls) => {
+      let user = [];
+      for(let i=0;i<usersUrls.length;i++){
+          try {
+          const res =  await fetch(usersUrls[i]);
+          const data = await res.json();
+          allUsers.current = data;
+          user.push( allUsers.current)
+          } catch (error) {
+            console.log(error)
+          }
+      }
+
+      setUsers(user)
+  }
+
+  useEffect(()=>{
+      fetchUsers()
+  }, [])
+
+  if(users.length==0) {
+    return (
+      <div className=''>
+        Loading Data...
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <SearchComponent setFilteredUsers={setFilteredUsers} users={users} setInputValue={setInputValue} />
+        <Userlist users={filteredUsers.length ==0 && !inputValue ? users : filteredUsers }/>
     </div>
   );
 }
